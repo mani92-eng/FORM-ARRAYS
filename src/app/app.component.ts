764b5a2c04c5d7134da7d756ebb234a6701a7a29
mani <pulikonda.manikanta@gmail.com>
2022-10-13 22:37:08 +0530

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormArray, FormArrayName, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Form';
+
+ testform=new FormGroup({
+
+  mobileName: new FormArray([
+     new FormControl(null,Validators.required),
+     new FormControl(null,Validators.required),
+   ])
+
+ })
+ Add(){
+ let mobileName = this.testform.get('mobileName') as FormArray
+ mobileName.push(new FormControl(null,Validators.required))
+ }
+ remove(i:number){
+  let mobileName=this.testform.get('mobileName') as FormArray
+  mobileName.removeAt(i)
+  console.log(mobileName)
+
+
+ }
 }
